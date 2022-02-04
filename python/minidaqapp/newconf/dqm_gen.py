a3f4e9047e298bcc68312a236142ed1c26555ee3
jmcarcell <jmcarcell@users.noreply.github.com>
2022-02-04 22:47:26 +0100

    Fix queue name for TRB

--- python/minidaqapp/newconf/dqm_gen.py
+++ python/minidaqapp/newconf/dqm_gen.py
@@ -64,7 +64,7 @@ class DQMApp(App):
 
         connections = {}
 
-        connections['output'] = Connection(f'trb_dqm.data_fragments_input_queue',
+        connections['output'] = Connection(f'trb_dqm.data_fragment_input_queue',
                                            queue_name='data_fragments_q',
                                            queue_kind='FollyMPMCQueue',
                                            queue_capacity=1000)
